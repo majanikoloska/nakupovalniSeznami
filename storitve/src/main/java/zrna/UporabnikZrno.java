2fd47f01ece9b0067dcb6099c99829aab8e0fee9
ma6068 <ma6068@student.uni-lj.si>
2019-11-10 23:18:35 +0100

    Testiranje

---
+++ storitve/src/main/java/zrna/UporabnikZrno.java
@@ -0,0 +1,21 @@
+package zrna;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import Entities.*;
+import javax.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UporabnikiZrno {
+
+    @PersistenceContext(unitName = "nakupovalniseznam-jpa")
+    private EntityManager em;
+
+    public List<UporabnikEntity> getUporabniki() {
+
+        List<UporabnikEntity> uporabniki = em.createNamedQuery(UporabnikEntity.GET_ALL).getResultList();
+        return uporabniki;
+    }
+}
