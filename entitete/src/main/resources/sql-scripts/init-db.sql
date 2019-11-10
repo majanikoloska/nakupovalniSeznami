2f314abc0724dd7e49d41e476fb511e27884865f
Maja <nikoloska_maja@hotmail.com>
2019-11-10 21:08:26 +0100

    Uporaba DataSource in priprave PU

---
+++ entitete/src/main/resources/sql-scripts/init-db.sql
@@ -0,0 +1,11 @@
+INSERT INTO uporabnik (name, lastname, username, email, password) VALUES ('Janez', 'Novak', 'janeznovak', 'janez.novak@hotmail.com','jn123321jn');
+INSERT INTO uporabnik (name, lastname, username, email, password) VALUES ('Jan', 'Kolar', 'jankolar', 'jan.kolar@gmail.com', 'kol123ar');
+INSERT INTO uporabnik (name, lastname, username, email, password) VALUES ('Lara', 'Kotnik', 'larakotnik', 'lara.kotnik@gmail.com', 'stol1233')
+INSERT INTO nakupovalniseznam (naziv, opis, status, opomba) VALUES ('Food', 'Saturday party', 'Pending', '');
+INSERT INTO nakupovalniseznam (naziv, opis, status, opomba) VALUES ('Clothes', 'New summer clothes', 'Pending', '');
+INSERT INTO artikel (naziv, zaloga) VALUES ('Red T-shirt', true);
+INSERT INTO artikel (naziv, zaloga) VALUES ('Denim shorts',true);
+INSERT INTO artikel (naziv, zaloga) VALUES ('Pizza', false);
+INSERT INTO artikel (naziv, zaloga) VALUES ('Hamburger', true);
+INSERT INTO artikel (naziv, zaloga) VALUES ('Chocolate', false);
+INSERT INTO artikel (naziv, zaloga) VALUES ('Beer', true);
