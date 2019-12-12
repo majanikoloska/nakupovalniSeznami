a22a7dc13761a886704a231fbcb97e75c5521110
ma6068 <ma6068@student.uni-lj.si>
2019-12-12 14:05:38 +0100

    shema za uporabnik

--- api/src/main/java/api/v1/viri/UporabnikiVir.java
+++ api/src/main/java/api/v1/viri/UporabnikiVir.java
@@ -4,6 +4,11 @@ import DTO.UporabnikDto;
 import Entities.UporabnikEntity;
 import api.v1.mappers.UporabnikMapper;
 import com.kumuluz.ee.rest.beans.QueryParameters;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import zrna.UporabnikZrno;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -29,6 +34,59 @@ public class UporabnikiVir {
 
     @Context
     protected UriInfo uriInfo;
+
+    @Schema(description = "Returns all users")
+    @SecurityRequirement(name = "none")
+    @Operation(summary = "Get customers list", tags = {"customers"}, description = "Returns a list of users.", responses = {
+            @ApiResponse(description = "List of users", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    UporabnikEntity.class)))
+    })
+
+    public Response vrniUporabniki(){
+        QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
+        List<UporabnikEntity> uporabniki = uporabnikZrno.getUporabniki2(query);
+        Long cnt = uporabnikZrno.pridobiUporabnikeCnt(query);
+        return Response.ok(uporabniki).header("X-Total-Count", cnt).build();
+    }
+
+    @Schema(description = "Add a user")
+    @SecurityRequirement(name = "none")
+    @Operation(summary = "Add user", tags = {"customers"}, description = "Adds a user.", responses = {
+            @ApiResponse(description = "Add user", responseCode = "201", content = @Content(schema = @Schema(implementation =
+                    UporabnikEntity.class)))
+    })
+
+    @POST
+    public Response dodajUporabnik(UporabnikEntity uporabnik){
+        uporabnikZrno.dodajUporabnik(uporabnik);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @Schema(description = "Update a user")
+    @SecurityRequirement(name = "none")
+    @Operation(summary = "Update a user", tags = {"customers"}, description = "Update a user by id.", responses = {
+            @ApiResponse(description = "Update a users", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    UporabnikEntity.class)))
+    })
+    @Path("{id}")
+    @PUT
+    public Response posodobiUporabnik(UporabnikEntity uporabnik){
+        uporabnikZrno.posodobiUporabnika(uporabnik);
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @Schema(description = "Deletes a user")
+    @SecurityRequirement(name = "none")
+    @Operation(summary = "Delete user", tags = {"customers"}, description = "Deletes a user by id.", responses = {
+            @ApiResponse(description = "Delete user", responseCode = "204", content = @Content(schema = @Schema(implementation =
+                    UporabnikEntity.class)))
+    })
+    @Path("{id}")
+    @DELETE
+    public Response odstraniUporabnika(@PathParam("id") Integer id) {
+        uporabnikZrno.izbrisiUporabnika(id);
+        return Response.status(Response.Status.GONE).build();
+    }
 
     @GET
     public List<UporabnikDto> vrniUporabnike() {
@@ -74,13 +132,6 @@ public class UporabnikiVir {
 
         uporabnikZrno.izbrisiUporabnika(uporabnikEntity.getId());
 
-    }
-
-    public Response vrniUporabniki(){
-        QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
-        List<UporabnikEntity> uporabniki = uporabnikZrno.getUporabniki2(query);
-        Long cnt = uporabnikZrno.pridobiUporabnikeCnt(query);
-        return Response.ok(uporabniki).header("X-Total-Count", cnt).build();
     }
 
 }
